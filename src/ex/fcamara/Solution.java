81b7c44f8e4f6d99e3f7f5c46cb1ccb631e6103d
jleme654 <julioromao3@gmail.com>
2016-07-28 10:51:25 -0300

    FCamara - exercicio diretrizes de Helicoptero

--- src/ex/fcamara/Solution.java
+++ src/ex/fcamara/Solution.java
@@ -2,37 +2,83 @@ package ex.fcamara;
 
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Julio Leme
+ * @since 2016-07-28
+ */
 public class Solution {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] arr = new int[] { 0, 0, 0 };
-		String test = sc.nextLine();
-		test = test.toUpperCase();
-		int count0 = 0, count1 = 0, count2 = 0;
-		for (int i = 0; i < arr.length; i++) {
-			for (int i2 = 0; i2 < test.length(); i2++) {
-				char c = test.charAt(i2);
-				if (c == 'L') {
-					arr[0] = count0++;
-					i2++;
-				}
-				if (c == 'R') {
-					count1 = count0;
-					arr[1] = count1++;
-					i2++;
-				}
-				if (c == 'D') {
-					arr[2] = count2--;
-					i2++;
-				}
+		String str = sc.nextLine();
+		str = str.toUpperCase();
+		int[] posicoes = new int[3];
+		
+		//posicoes iniciais x=0, y=0, z=1; e contadores de subidas e descidas
+		int countX = 0, countY=0, countZ = 0, countD=0, countU=0;
+		
+		for (int i = 1; i < str.length()-1; i++) {
+			char cAnterior = str.charAt(i-1);
+			char cPosterior = str.charAt(i);
+			//char cPosteriorPost = str.charAt(i+1);
+			
+			//eixo X-------------------------------
+			if (cAnterior == 'L' && cPosterior == 'M') {
+				posicoes[0] = countX++;
+				//posicoes[1] = countY--;
+				//posicoes[2]--;
 			}
+			else if (cAnterior == 'M'){
+				posicoes[0] = countX++;
+			}
+			//eixo Y--------------------------------
+			if (cAnterior == 'L' && cPosterior == 'M') {
+				//posicoes[0]--;
+				posicoes[1] = countY++;
+				//posicoes[2]--;
+			}
+			else if (cAnterior == 'M'){
+				posicoes[0]--;
+				posicoes[1] = countY++;
+				//posicoes[2]--;
+			}
+			//eixo Z---------------------------------
+			if (cPosterior == 'D') {
+				posicoes[0]--;
+				posicoes[1]--;
+				countZ = countZ -1;
+				posicoes[2] = countZ;
+				//conta a qtde de descidas do helicoptero
+				countD++;
+			}
+			if (cPosterior == 'U') {
+				posicoes[0]--;
+				posicoes[1]--;
+				countZ = countZ + 1;
+				posicoes[2] = countZ;
+				//conta a qtde de subidas do helicoptero
+				countU++;
+			}	
 		}
-		if (test.contains("U")) {
-			System.out.print((arr[0] - 1) + "\t" + (arr[1] - 1) + "\t" + (arr[2] + 1) + "\t" + "NORTE");
-		} else {
-			System.out.print((arr[0] - (arr[1] - 1)) + "\t" + (arr[1] - 1) + "\t" + (arr[2]) + "\t" + "SUL");
+		
+		for (int i = 0; i < posicoes.length; i++) {
+			System.out.print(posicoes[i] + "\t");
 		}
-	}
 
+		//ultimo elemento da string
+		String s = str.substring(str.length()-1, str.length());
+		if (s.equals("U")) 
+			countU++;
+		else
+		    countD++;
+		
+		if (countU >= countD) 
+			System.out.println("NORTE");
+		else
+			System.out.println("SUL");
+		
+	}
+	
+	
 }
---
+++ src/ex/fcamara/Solution2.java
@@ -0,0 +1,38 @@
+package ex.fcamara;
+
+import java.util.Scanner;
+
+public class Solution2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[] { 0, 0, 0 };
+		String test = sc.nextLine();
+		test = test.toUpperCase();
+		int count0 = 0, count1 = 0, count2 = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int i2 = 0; i2 < test.length(); i2++) {
+				char c = test.charAt(i2);
+				if (c == 'L') {
+					arr[0] = count0++;
+					i2++;
+				}
+				if (c == 'R') {
+					count1 = count0;
+					arr[1] = count1++;
+					i2++;
+				}
+				if (c == 'D') {
+					arr[2] = count2--;
+					i2++;
+				}
+			}
+		}
+		if (test.contains("U")) {
+			System.out.print((arr[0] - 1) + "\t" + (arr[1] - 1) + "\t" + (arr[2] + 1) + "\t" + "NORTE");
+		} else {
+			System.out.print((arr[0] - (arr[1] - 1)) + "\t" + (arr[1] - 1) + "\t" + (arr[2]) + "\t" + "SUL");
+		}
+	}
+
+}
